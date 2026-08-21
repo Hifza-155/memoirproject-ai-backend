9e8386a1b3a2956df9fe2900f93710d1f44df635
Hifza-155 <hafsausman1512@gmail.com>
2026-08-21 12:13:15 +0500

    Health Check endpoint added this will be used by docker container to verify app is alive

--- src/main.py
+++ src/main.py
@@ -5,3 +5,9 @@ app = FastAPI(title="Memoir App API")
 @app.get("/")
 def read_root():
     return {"message": "API is successfully running!"}
+
+# 3. Health Check Endpoint
+@app.get("/health")
+def health_check():
+    """Used by Docker containers and cloud hosting to verify the app is alive."""
+    return {"status": "healthy"}
