45bf5e878788db5920382c09ff1b4e8fe153f3d6
Hifza-155 <hafsausman1512@gmail.com>
2026-08-21 12:23:25 +0500

    CORS configuration added so only our frontend can talk to backend

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Memoir App API") 
 
+#CORS Configuration
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Allows all request headers
+)
+
+# Root endpoint
 @app.get("/")
 def read_root():
     return {"message": "API is successfully running!"}
 
-# 3. Health Check Endpoint
+# Health Check Endpoint
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
